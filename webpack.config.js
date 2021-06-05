a239f4822016716c32150709692bc547520c233e
Gonzo <gvidal.industrial@gmail.com>
2021-06-05 16:26:24 -0500

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions:  [".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin([
+      {
+        inject: true,
+        template: "./public/index.html",
+        filename: "./index.html"
+      }
+    ])
+  ]
+}
